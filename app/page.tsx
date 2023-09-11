9c6aed7469b2f74ed03c588c34afe1efa41bd421
premma <premma1300@gmail.com>
2023-09-11 16:27:51 +0700

    update navbar and home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,15 @@
-'use client';
-
-import { Button } from '@/components/ui/button';
-
 export default function Home() {
   return (
     <main>
-      <Button variant='default'>click me</Button>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam quis tortor euismod dolor iaculis
+      pellentesque a non eros. Nunc in ex molestie, dapibus massa ut, facilisis enim. Aliquam egestas
+      convallis libero, in elementum eros laoreet sit amet. Nulla vestibulum, orci vel semper sodales, quam
+      risus elementum leo, eget pulvinar eros nibh vitae justo. Morbi fermentum sem eu ipsum ornare, et
+      lacinia tortor iaculis. Vestibulum lobortis ante vitae leo posuere vehicula. Suspendisse vel suscipit
+      velit. In at ornare risus. Sed eget lectus dictum felis dignissim malesuada. Suspendisse ornare laoreet
+      pulvinar. Proin sollicitudin neque nunc, ac elementum ligula iaculis vel. Duis eu ipsum est. Quisque at
+      quam sapien. Mauris posuere gravida nunc vitae venenatis. Quisque at tempus justo. In auctor ac mi vitae
+      bibendum.
     </main>
   );
 }
--- components/nav/index.tsx
+++ components/nav/index.tsx
@@ -3,8 +3,8 @@ import Link from 'next/link';
 const Nav = () => {
   return (
     <header>
-      <nav>
-        <ul>
+      <nav className='mb-5'>
+        <ul className='flex justify-around p-5 bg-slate-600 text-zinc-50'>
           <li className='hover:underline'>
             <Link href='/'>Home</Link>
           </li>
