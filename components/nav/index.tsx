d29d811f7610b7864ffe60e2c7a4c96e98c6f70d
premma <premma1300@gmail.com>
2024-04-06 12:18:31 +0700

    test middleware

--- components/nav/index.tsx
+++ components/nav/index.tsx
@@ -1,16 +1,30 @@
 import Link from 'next/link';
 
+const menuItems: { name: string; href: string }[] = [
+  {
+    name: 'Home',
+    href: '/',
+  },
+  {
+    name: 'Users',
+    href: '/users',
+  },
+  {
+    name: 'Posts',
+    href: '/posts',
+  },
+];
+
 const Nav = () => {
   return (
     <header>
       <nav className='mb-5'>
         <ul className='flex justify-around p-5 bg-slate-600 text-zinc-50'>
-          <li className='hover:underline'>
-            <Link href='/'>Home</Link>
-          </li>
-          <li>
-            <Link href='/users'>Users</Link>
-          </li>
+          {menuItems.map(menuItem => (
+            <li className='hover:underline' key={menuItem.href}>
+              <Link href={menuItem.href}>{menuItem.name}</Link>
+            </li>
+          ))}
         </ul>
       </nav>
     </header>
