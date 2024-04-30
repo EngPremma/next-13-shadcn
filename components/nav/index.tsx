a7698b5bbc540cc59bc6ee3d23c05c8cde4998d8
premma <premma1300@gmail.com>
2024-04-30 22:27:06 +0700

    test route groups

--- components/nav/index.tsx
+++ components/nav/index.tsx
@@ -13,13 +13,17 @@ const menuItems: { name: string; href: string }[] = [
     name: 'Posts',
     href: '/posts',
   },
+  {
+    name: 'Admin',
+    href: '/admin',
+  },
 ];
 
 const Nav = () => {
   return (
     <header>
       <nav className='mb-5'>
-        <ul className='flex justify-around p-5 bg-slate-600 text-zinc-50'>
+        <ul className='flex justify-around p-5 bg-amber-700 text-zinc-50'>
           {menuItems.map(menuItem => (
             <li className='hover:underline' key={menuItem.href}>
               <Link href={menuItem.href}>{menuItem.name}</Link>
